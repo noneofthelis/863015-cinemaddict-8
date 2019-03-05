f12db400df05ecf35477861a71f2b5672a9359a2
noneofthelis <alisa.snag@gmail.com>
2019-03-05 13:48:34 +0100

    removed commented out

--- src/main.js
+++ src/main.js
@@ -1,32 +1,9 @@
-/* 1. Подключите файл bundle.js из директории public в конце файла public/index.html с помощью тега <script>.
-
-2. Ознакомьтесь с разметкой в index.html. Ваша задача: подготовить шаблоны для описания фильтров и карточек фильмов.
-
-3. В директории src/ создайте модуль и опишите в нем функцию для отрисовки отдельного фильтра.
-Функция должна уметь отрисовывать любой фильтр, предусмотренный макетом.
-
-4. Создайте еще один модуль с функцией для отрисовки одной карточки фильма. Пока у нас нет данных о реальных
-фильмах, пусть функция формирует шаблон карточки опираясь на данные из макета.
-
-5. В файле main.js подключите созданные модули.
-
-6. При помощи функции, описанной в пункте 3 отрисуйте все фильтры, предусмотренные макетом:
-«All movies», «Watchlist», «History», «Favorites». Не забудьте возле фильтров «Watchlist», «History» и «Favorites»
-вывести произвольное число, которое будет притворяться количеством фильмов, пока у нас нет настоящих данных..
-
-7. Используя функцию из пункта 4 отрисуйте семь одинаковых карточек фильмов.
-Ещё по две карточки отрисуйте в блоки «Top rated» и «Most commented»
-
-8. Добавьте обработчик для переключения фильтров. При их переключении удаляйте все ранее созданные фильмы
- и добавляйте случайное количество новых.*/
-
 import createFilter from './create-filter.js';
 import createCard from './create-card.js';
 
 const filtersContainer = document.querySelector(`.main-navigation`);
 const CardsContainer = document.querySelector(`.films`);
 const mainCardsContainer = CardsContainer.querySelector(`.films-list__container`);
-
 const mostRated = CardsContainer.children[1].querySelector(`.films-list__container`);
 const mostCommented = CardsContainer.children[2].querySelector(`.films-list__container`);
 
